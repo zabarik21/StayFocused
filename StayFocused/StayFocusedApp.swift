5a7317d0fe832a8cb79112104699a387182cde8e
zabarik21 <mityafara426@gmail.com>
2022-10-05 11:30:04 +0500

    Initial Commit

---
+++ StayFocused/StayFocusedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayFocusedApp.swift
+//  StayFocused
+//
+//  Created by Timofey on 5/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct StayFocusedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
